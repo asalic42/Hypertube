a9246681e19456bb293397eaebffe9a3c9867af8
aurore-dcb <auroreducobu@gmail.com>
2026-07-08 17:54:16 +0200

    api swagger

--- volumes/app-users/config/urls.py
+++ volumes/app-users/config/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.http import JsonResponse
 from django.urls import include, path
 
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
 
 def root(request):
     return JsonResponse({"service": "app-users-api", "status": "ok"})
@@ -10,5 +12,11 @@ def root(request):
 urlpatterns = [
     path("", root),
     path("api/", include("api.urls")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
     path("admin/", admin.site.urls),
 ]
