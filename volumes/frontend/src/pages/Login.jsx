49d53d67975608ce42634e6db28829916dd80680
Anthony Ciezadlik <150702158+aciezadl@users.noreply.github.com>
2026-07-29 12:15:00 +0200

    up

--- volumes/frontend/src/pages/Login.jsx
+++ volumes/frontend/src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import './Auth.css'
+// import './Auth.css'
 
 export default function Login() {
     const [formData, setFormData] = useState({email: '', password: ''});
