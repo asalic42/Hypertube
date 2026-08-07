51fdfc5c1ca8845ce6232256c84d1acc65dd2e46
aurore-dcb <auroreducobu@gmail.com>
2026-08-07 14:32:20 +0200

    init bucket s3

--- services/file-storage/init/init.py
+++ services/file-storage/init/init.py
@@ -16,11 +16,30 @@ def create_bucket(bucket_name):
         ),
     )
     try:
+        buckets = s3.list_buckets()
+        if any(bucket['Name'] == bucket_name for bucket in buckets['Buckets']):
+            print(f'🟰  Bucket {bucket_name} already exists.')
+            return
         s3.create_bucket(Bucket=bucket_name)
-        print(f'Bucket {bucket_name} created.')
-    except s3.exceptions.BucketAlreadyOwnedByYou:
-        print(f'Bucket {bucket_name} already exists.')
+        print(f'✅ Bucket {bucket_name} created.')
+    except ClientError as e:
+        error_code = e.response["Error"]["Code"]
+        print(f"Error creating bucket {bucket_name}: {error_code}")
+
+        if error_code in [
+            "BucketAlreadyExists",
+            "BucketAlreadyOwnedByYou"
+        ]:
+            print(f"🟰  Bucket {bucket_name} already exists.")
+        else:
+            raise
 
 
-create_bucket('profil-pics')
-create_bucket('movies')
+if __name__ == "__main__":
+    try:
+        with open("/init/buckets.txt", "r") as f:
+            buckets = [line.strip() for line in f.readlines() if line.strip()]
+        for bucket in buckets:
+            create_bucket(bucket)
+    except FileNotFoundError:
+        print("⚠️  buckets.txt file not found. No buckets to create. ⚠️")
